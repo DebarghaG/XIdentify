aece73fc631ce77deda5b78f9e95671dc8567b6b
DebarghaG <maildebargha@gmail.com>
2020-09-17 13:03:23 +0530

    API functionality for basic explaination now works

--- src/main.py
+++ src/main.py
@@ -1,25 +1,23 @@
-from PIL import Image
-import torch.nn as nn
-import numpy as np
 import os
-import json
-import matplotlib.pyplot as plt
-import torch
-from torchvision import models, transforms
-from torch.autograd import Variable
-import torch.nn.functional as F
+import process_image
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from werkzeug.middleware.shared_data import SharedDataMiddleware
+
+
+
 
 UPLOAD_FOLDER = './uploads/'
-ALLOWED_EXTENSIONS = {'txt', 'png', 'jpg', 'jpeg'}
+ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -37,8 +35,11 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            print(filename)
+            process_image.analyze(filename)
+            filename = "./processed"+filename
+            return redirect(url_for('uploaded_file',filename=filename))
+
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -49,133 +50,12 @@ def upload_file():
     </form>
     '''
 
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-
-from werkzeug.middleware.shared_data import SharedDataMiddleware
-app.add_url_rule('/uploads/<filename>', 'uploaded_file',build_only=True)
-app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {'/uploads':  app.config['UPLOAD_FOLDER']})
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from lime import lime_image
-
-def get_image(path):
-    with open(os.path.abspath(path), 'rb') as f:
-        with Image.open(f) as img:
-            return img.convert('RGB')
-
-# resize and take the center part of image to what our model expects
-def get_input_transform():
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                    std=[0.229, 0.224, 0.225])
-
-    transf = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.CenterCrop(224),
-        transforms.ToTensor()
-    ])
-
-    return transf
-
-def get_input_tensors(img):
-    transf = get_input_transform()
-    # unsqeeze converts single image to batch of 1
-    return transf(img).unsqueeze(0)
-
-
-def batch_predict(images):
-    model.eval()
-    batch = torch.stack(tuple(preprocess_transform(i) for i in images), dim=0)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-    batch = batch.to(device)
-
-    logits = model(batch)
-    probs = F.softmax(logits, dim=1)
-    return probs.detach().cpu().numpy()
-
-def get_pil_transform():
-    transf = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.CenterCrop(224)
-    ])
-
-    return transf
-
-def get_preprocess_transform():
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                    std=[0.229, 0.224, 0.225])
-    transf = transforms.Compose([
-        transforms.ToTensor(),
-        normalize
-    ])
-
-    return transf
-
-
-print("Getting Inception Model")
-model = models.inception_v3(pretrained=True)
-img = get_image('./uploads/adv_panda.png')
-plt.imshow(img)
-idx2label, cls2label, cls2idx = [], {}, {}
-with open(os.path.abspath('imagenet_class_index.json'), 'r') as read_file:
-    class_idx = json.load(read_file)
-    idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
-    cls2label = {class_idx[str(k)][0]: class_idx[str(k)][1] for k in range(len(class_idx))}
-    cls2idx = {class_idx[str(k)][0]: k for k in range(len(class_idx))}
-
-
-img_t = get_input_tensors(img)
-model.eval()
-logits = model(img_t)
-
-probs = F.softmax(logits, dim=1)
-probs5 = probs.topk(5)
-print(tuple((p,c, idx2label[c]) for p, c in zip(probs5[0][0].detach().numpy(), probs5[1][0].detach().numpy())))
-
-
-
-
-pill_transf = get_pil_transform()
-preprocess_transform = get_preprocess_transform()
-
-
-"""
-test_pred = batch_predict([pill_transf(img)])
-test_pred.squeeze().argmax()
-explainer = lime_image.LimeImageExplainer()
-explanation = explainer.explain_instance(np.array(pill_transf(img)),
-                                         batch_predict, # classification function
-                                         top_labels=5,
-                                         hide_color=0,
-                                         num_samples=1000)
-from skimage.segmentation import mark_boundaries
-temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
-img_boundry1 = mark_boundaries(temp/255.0, mask)
-plt.imshow(img_boundry1)
-
-temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10, hide_rest=False)
-img_boundry2 = mark_boundaries(temp/255.0, mask)
-plt.imshow(img_boundry2)
-"""
+app.add_url_rule('/uploads/<filename>', 'uploaded_file', build_only=True)
+app.wsgi_app = SharedDataMiddleware(
+    app.wsgi_app, {'/uploads':  app.config['UPLOAD_FOLDER']})
